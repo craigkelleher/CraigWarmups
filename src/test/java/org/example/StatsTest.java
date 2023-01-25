e98287f41db6ae1b17429423d77672248fc88276
Craig Kelleher <kelleher.craig@gmail.com>
2023-01-25 07:32:18 -0800

    Warmup2: Stats & StatsTest Classes created

---
+++ src/test/java/org/example/StatsTest.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StatsTest {
+    public int computeMode(List<Integer> data){
+        // returns the most frequently appearing value in the given data parameter
+
+        return 0;
+    }
+}
