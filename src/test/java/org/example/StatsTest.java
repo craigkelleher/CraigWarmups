e73f54355286d5d53948b5189eb82c3503c9d95c
Craig Kelleher <kelleher.craig@gmail.com>
2023-01-25 07:49:19 -0800

    Warmup2: Implement StatsTest computeModeTest

--- src/test/java/org/example/StatsTest.java
+++ src/test/java/org/example/StatsTest.java
@@ -1,15 +1,26 @@
 package org.example;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsTest {
-    public int computeMode(List<Integer> data){
-        // returns the most frequently appearing value in the given data parameter
+//
+//    @BeforeEach
+//    void setUp(){
+//        Stats stats = new Stats();
+//    }
 
-        return 0;
+    @Test
+    public void computeModeTest(){
+        List<Integer> compareData = new ArrayList<>();
+        compareData.add(2);
+        compareData.add(3);
+        compareData.add(4);
+        compareData.add(8);
+        compareData.add(7);
+        compareData.add(8);
+        assertEquals(8, Stats.computeMode(compareData));
     }
 }
