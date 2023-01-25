622e9aedca86dd35781b6b2df16b105d136bc323
Craig Kelleher <kelleher.craig@gmail.com>
2023-01-25 07:49:45 -0800

    Warmup2: Implement Stats class, computeMode method

--- src/main/java/org/example/Stats.java
+++ src/main/java/org/example/Stats.java
@@ -1,5 +1,31 @@
 package org.example;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class Stats {
 
+    public static int computeMode(List<Integer> data) {
+        Map<Integer, Integer> dataCompare = new HashMap<>();
+        int numCount = 0;
+        int mode = 0;
+
+        for(int num : data){
+            if (dataCompare.containsKey(num)){
+                dataCompare.put(num, dataCompare.get(num) +1);
+            }
+            else{
+                dataCompare.put(num, 1);
+            }
+        }
+
+        for(Map.Entry<Integer, Integer> numbers : dataCompare.entrySet()){
+            if(numbers.getValue() > numCount){
+                numCount = numbers.getValue();
+                mode = numbers.getKey();
+            }
+        }
+        return mode;
+    }
 }
